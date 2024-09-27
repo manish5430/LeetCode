48951205cb22213cc188c242e74e5b6905f52e3c
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-09-27 18:24:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (87.51%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -1,28 +1,29 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        ArrayList<Integer> uniqueList = new ArrayList<>();
+        long num1 = Long.MIN_VALUE;
+        long num2 = Long.MIN_VALUE;
+        long num3 = Long.MIN_VALUE;
 
-        // Step 1: Collect unique numbers
-        for (int num : nums) {
-            if (!uniqueList.contains(num)) {
-                uniqueList.add(num);
+        for(int num : nums){
+            if( num > num1){
+                num3 = num2;
+                num2 = num1;
+                num1 = num;
+            }
+            else if( num > num2 && num < num1){
+                num3 = num2;
+                num2 = num;
+            }
+            else if( num > num3 && num < num2){
+                num3 = num;
             }
         }
 
-        // Step 2: Check if we have at least 3 unique numbers
-        if (uniqueList.size() < 3) {
-            // Find the maximum manually
-            int max = uniqueList.get(0);
-            for (int i = 1; i < uniqueList.size(); i++) {
-                if (uniqueList.get(i) > max) {
-                    max = uniqueList.get(i);
-                }
-            }
-            return max; // Return the maximum
+        if(num3 != Long.MIN_VALUE){
+            return (int)num3;
         }
-
-        // Step 3: Sort the list and return the third maximum
-        uniqueList.sort(null); // Sort the list
-        return uniqueList.get(uniqueList.size() - 3); // Return the third maximum
+        else{
+            return (int)num1;
+        }
     }
 }
