1ca5310aef8cebe3a68f4c7ddc1a0d87bd5a70b0
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-09-28 18:57:25 +0530

    Time: 4 ms (85.30%) | Memory: 50.3 MB (91.05%) - LeetSync

---
+++ 1899-count-items-matching-a-rule/count-items-matching-a-rule.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countMatches( List<List<String>> items, String ruleKey, String ruleValue) {
+        int i = 0;;
+
+        if(ruleKey.equals("type"))  i = 0;
+        else if( ruleKey.equals("color"))  i = 1;
+        else if( ruleKey.equals("name"))  i = 2;
+
+        int count = 0;
+        for( List<String> num : items ){
+            if( ruleValue.equals(num.get(i))){
+                count++;
+            }
+        }
+        return count;
+    }
+}
