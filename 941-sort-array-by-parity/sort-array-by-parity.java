cfafea315fb11208f673cf9cb5a35f51624b1655
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-09-30 17:59:37 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (89.61%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int i = 0;                  
+        int j = nums.length - 1;   
+        while (i < j) {
+            if (nums[i] % 2 == 0) {
+                i++;
+            } else if (nums[j] % 2 != 0) { 
+                j--;
+            } else { 
+                swap(nums, i, j);
+                i++;
+                j--;
+            }
+        }
+
+        return nums;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
