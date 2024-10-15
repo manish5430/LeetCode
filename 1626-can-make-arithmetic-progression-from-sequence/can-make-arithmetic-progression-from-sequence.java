193022b2d87765a80beb2c72360f78ed5081d325
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-10-15 18:03:10 +0530

    Time: 2 ms (97.12%) | Memory: 42.1 MB (14.46%) - LeetSync

---
+++ 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+
+        if(arr.length <= 2){
+            return true;
+        }
+       
+        for(int i =0; i< arr.length-2; i++){
+            if((arr[i+1] - arr[i]) != (arr[i+2] - arr[i+1])){
+            return false;
+            }
+        }
+        
+        return true;
+    }
+}
