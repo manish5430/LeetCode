3a4d5ec1b618de6ce3d3dce9a0d3004aafc733ed
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-10-15 21:45:04 +0530

    Time: 12 ms (37.58%) | Memory: 45.3 MB (72.11%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,32 +1,31 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         ArrayList<Integer> list = new ArrayList<>();
-        
-        for(int i=0; i< nums.length;){
-            int correct = nums[i] -1;
+        Arrays.sort(nums);
 
-            if(nums[i] != nums[correct]){
-                swap(nums, i , correct);
+        for(int i= 0; i< nums.length;){
+            int index = nums[i] -1;
+            if(nums[i] != nums[index]){
+                swap(nums, i, index);
             }
             else{
                 i++;
             }
         }
         for(int i=0; i< nums.length; i++){
-            if(nums[i] != (i+1)){
+            if( nums[i] != (i +1)){
                 list.add(nums[i]);
                 list.add(i+1);
             }
         }
         int[] ans = new int[list.size()];
-        for(int i = 0; i< ans.length; i++){
+        for(int i=0; i< ans.length; i++){
             ans[i] = list.get(i);
         }
         return ans;
     }
-    
-    static void swap(int[] nums, int start, int end ){
-        int temp = nums[start];
+    static void swap(int[] nums, int start,int end){
+        int temp = nums[start]; 
         nums[start] = nums[end];
         nums[end] = temp;
     }
