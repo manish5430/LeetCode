3e850393df5d82a4693feab589310ba0e9a54a34
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-11-17 17:16:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (47.51%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.java
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -2,12 +2,15 @@ class Solution {
     public String sortSentence(String s) {
         String[] str = s.split(" ");
         String[] arr = new String[str.length];
-
         for(String word : str){
-            int n = word.length();
-            int numeric = word.charAt(n -1) - '0';
-            arr[numeric -1] = word.substring(0, n-1);
+           int n = word.length();
+
+           int num = word.charAt(n-1) -'0';
+           arr[num -1] = word.substring(0, n-1);
         }
+
         return String.join(" ", arr);
+
+
     }
 }
