db427f4c3873b10023d6b6fef7fd25d4a241fcdc
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-11-24 21:08:23 +0530

    Time: 1 ms (79.06%) | Memory: 41.4 MB (67.06%) - LeetSync

--- 961-long-pressed-name/long-pressed-name.java
+++ 961-long-pressed-name/long-pressed-name.java
@@ -2,25 +2,20 @@ class Solution {
     public boolean isLongPressedName(String name, String typed) {
         int i = 0, j = 0;
 
-        while(i < name.length() && j < typed.length()){
-            if(name.charAt(i) == typed.charAt(j)){
-                i++; j++;
-            }else if( i> 0  && name.charAt(i-1) == typed.charAt(j)){
-                j++;
-            }
-            else{
+        while (j < typed.length()) {
+            // If characters match in both name and typed, or if it's a valid long press (typed matches previous name char)
+            if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
+                i++;  // Move to the next character in name
+            } else if (i > 0 && name.charAt(i - 1) == typed.charAt(j)) {
+                // If typed character matches the previous character in name, move to the next character in typed
+            } else {
+                // If neither condition is met, return false
                 return false;
             }
+            j++;  // Move to the next character in typed
         }
 
-        while(j< typed.length()){
-            if( typed.charAt(j) != name.charAt(i-1)){
-                return false;
-            }else{
-                j++;
-            }
-        }
+        // If we've matched all characters in name, return true
         return i == name.length();
-               
     }
 }
