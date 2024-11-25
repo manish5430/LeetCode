86df65d53190cfc47a1d38a599afe5fdf852dc04
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-11-25 10:26:22 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (44.86%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,18 +1,17 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-       Arrays.sort(strs);
-       String s1 = strs[0];
-       String s2 = strs[strs.length-1];
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+        int i = 0;
 
-       int index = 0;
-       while(index < s1.length() && index < s2.length()){
-            if(s1.charAt(index) == s2.charAt(index)){
-                index++;
+        while( i< s1.length() &&  i < s2.length()){
+            if(s1.charAt(i) == s2.charAt(i)){
+                i++;
+            }else{
+                 break;
             }
-            else{
-                break;
-            }
-       }
-       return s1.substring(0, index);       
+        }
+        return s1.substring(0, i);
     }
 }
