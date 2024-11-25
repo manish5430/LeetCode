e8db9ea97484cd769f1f601b96ae3b57d8725941
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-11-25 12:35:37 +0530

    Time: 1 ms (60.05%) | Memory: 41.4 MB (91.44%) - LeetSync

--- 1764-maximum-repeating-substring/maximum-repeating-substring.java
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.java
@@ -1,22 +1,11 @@
 class Solution {
     public int maxRepeating(String s, String word) {
-        if(!s.contains(word)){
-            return 0;
-        }
         int count = 0;
-        int i = 0;
-        while(i<= s.length()-word.length()){
-            int count1 = 0;
-            while(i<= s.length()-word.length()   &&   s.substring(i, i + word.length()).equals(word)){
-                count1++;
-                i = i+word.length();
-            }
-            count = Math.max(count, count1);
-            if(count1 == 0){
-                i++;
-            }else{
-                i = i- (word.length()-1);
-            }
+        String w1 = word;
+
+        while( s.contains(w1)){
+            w1 += word;
+            count++;
         }
         return count;
     }
