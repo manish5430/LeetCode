bf31c409fe7b4fb97c917f38bfac462e3c782e12
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-11-26 19:25:09 +0530

    Time: 18 ms (5.49%) | Memory: 45.2 MB (7.02%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
@@ -2,9 +2,9 @@ class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
         for(int i= 0; i< s1.length(); i++){
             for(int j = 0; j< s1.length(); j++){
-                String s = swap(s1, i, j);
+                //String s = swap(s1, i, j);
             
-                if(s.equals(s2)){
+                if(swap(s1, i, j).equals(s2)){
                    return true;
                 }
             }
