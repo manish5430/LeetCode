0842659de2af95d42ee2f045f8fe9b125e3695b8
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-11-27 20:08:20 +0530

    Time: 2 ms (74.03%) | Memory: 42.4 MB (45.79%) - LeetSync

--- 889-buddy-strings/buddy-strings.java
+++ 889-buddy-strings/buddy-strings.java
@@ -16,17 +16,20 @@ class Solution {
 
         int first = -1;
         int second = -1;
+        int mismatched = 0;
         for(int i = 0; i< s.length(); i++){
             if(s.charAt(i) != goal.charAt(i)){
-                if(first == -1){
-                    first = i;
-                }else if(second == -1){
-                    second = i;
-                }else{
+                mismatched++;
+                if(mismatched > 2){
                     return false;
-                }
+                }else{
+                    if(first == -1) first = i;
+                    second = i;
+                }                
             }
         }
-        return second != -1 && s.charAt(first) == goal.charAt(second) && s.charAt(second) == goal.charAt(first);
+        if(mismatched == 2)
+        return s.charAt(first) == goal.charAt(second) && s.charAt(second) == goal.charAt(first);
+        return false;
     }
 }
