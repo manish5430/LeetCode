58e6d02a23d324eecbae6428c7cb6470f52909b3
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-01 15:23:01 +0530

    Time: 278 ms (12.62%) | Memory: 58.4 MB (6.56%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,44 +1,39 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        mergeSort(nums, 0, nums.length-1);
+        // solving with quickSort 
+        quickSort(nums, 0, nums.length-1);
         return nums;
-        
+
     }
 
-    static void mergeSort(int[] nums, int start, int end){
+    static void quickSort(int[] nums, int start, int end){
         if(start >= end) return;
-
-        int mid = start + (end- start)/2;
-
-        mergeSort(nums, start, mid);
-        mergeSort(nums, mid+1, end);
-
-        merge(nums, start, mid, end);
-    }
-
-    static void merge(int[] nums, int start, int mid, int end){
-        int[] result = new int[end-start +1];
-
+        
         int i = start;
-        int j = mid+1;
-        int k = 0;
+        int j = end;
+        int mid = i + (j -i)/2;
+        int pivot = nums[mid];
 
-        while(i<= mid && j <= end){
-            if(nums[i] < nums[j]){
-                result[k++] = nums[i++];
-            }else{
-                result[k++] = nums[j++];
+        while(i <= j){
+            while(nums[i] < pivot){
+                i++;
+            }
+            while(nums[j] > pivot){
+                j--;
+            }
+
+            if(i <= j){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
             }
         }
-        while(i <= mid){
-            result[k++] = nums[i++];
-        }
-        while(j <= end){
-            result[k++] = nums[j++];
-        }
 
-        for(int m = 0; m< result.length; m++){
-            nums[start +m] = result[m];
-        }
+        quickSort(nums, start, j);
+        quickSort(nums, i, end);
+
+        
     }
 }
