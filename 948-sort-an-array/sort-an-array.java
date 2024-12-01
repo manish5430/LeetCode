1004b3fcc4c3ef0f48ce3c394f33e20accf6be72
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-01 11:10:33 +0530

    Time: 28 ms (48.35%) | Memory: 56 MB (63.34%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,6 +1,46 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
-        return nums;
+        return mergeSort(nums);
+        
+    }
+
+    static int[] mergeSort(int[] nums){
+        if(nums.length == 1)  return nums;
+        int mid = nums.length /2;
+
+        int[] first = mergeSort(Arrays.copyOfRange(nums, 0, mid));
+        int[] second = mergeSort(Arrays.copyOfRange(nums, mid, nums.length));
+
+        return merge(first, second);
+    }
+
+    static int[] merge(int[] first, int[] second){
+        int[] result = new int[first.length + second.length];
+
+        int i = 0; 
+        int j = 0;
+        int k = 0;
+
+        while(i < first.length && j < second.length){
+            if(first[i] < second[j]){
+                result[k] = first[i];
+                i++;
+            }else{
+                result[k] = second[j];
+                j++;
+            }
+            k++;
+        }
+        while(i< first.length){
+            result[k] = first[i];
+            i++;
+            k++;
+        }
+        while(j< second.length){
+            result[k] = second[j];
+            j++;
+            k++;
+        }
+        return result;
     }
 }
