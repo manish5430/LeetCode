b44d4d419e632209114f26bbd7004c1981c946f0
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-02 10:54:47 +0530

    Time: 36 ms (30.87%) | Memory: 51.8 MB (39.33%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -14,8 +14,8 @@ class Solution {
                 if(sum == 0){
                     setTriplet.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                    // while(left< right && nums[left] == nums[left+1]) left++;
-                    // while(left< right && nums[right] == nums[right-1]) right--;
+                    while(left< right && nums[left] == nums[left+1]) left++;
+                    while(left< right && nums[right] == nums[right-1]) right--;
 
                     left++;
                     right--;
