1f58b2f5c3ea8d941a75f6a705b0fa499c25e962
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-05 11:58:05 +0530

    Time: 4 ms (6.16%) | Memory: 45.5 MB (5.59%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -3,14 +3,12 @@ class Solution {
         ArrayList<Integer> list = new ArrayList<>();
         for(int n : nums){
             if(n %2 == 0){
+                list.add(0, n);
+            }else{
                 list.add(n);
             }
         }
-        for(int n : nums){
-            if( n%2 != 0){
-                list.add(n);
-            }
-        }
+        
         int[] ans = new int[list.size()];
         for(int i = 0; i< ans.length; i++){
             ans[i] = list.get(i);
