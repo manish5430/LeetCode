e13f5b8d8009a0518e2ca657cf67a46624cf48ba
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-05 20:40:56 +0530

    Time: 1 ms (99.15%) | Memory: 44.7 MB (66.03%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,28 +1,37 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        if(intervals.length == 0) return new int[][] {newInterval};
-
-        List<int[]> list = new ArrayList<>();
+        ArrayList<int[]> list = new ArrayList<>();
 
         int i = 0;
-        while(i< intervals.length && intervals[i][1] < newInterval[0]){
-            list.add(intervals[i]);
-            i++;
+        while(i < intervals.length){
+            if(intervals[i][0] < newInterval[0]){
+                list.add(intervals[i]);
+                i++;
+            }else{
+                break;
+            }
         }
 
-        while(i< intervals.length && newInterval[1] >= intervals[i][0]){
-            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-            i++;
+        if(list.size() == 0  || list.get(list.size()-1)[1] < newInterval[0]){
+            list.add(newInterval);
         }
-
-        list.add(newInterval);
+        else{
+            int[] preInterval = list.get(list.size()-1);
+            preInterval[1] = Math.max(preInterval[1], newInterval[1]);
+        }
 
         while(i < intervals.length){
-            list.add(intervals[i]);
+            int[] preInterval = list.get(list.size()-1);
+            if(preInterval[1] >= intervals[i][0]){
+                preInterval[1] = Math.max(preInterval[1], intervals[i][1]);
+            }
+            else{
+                list.add(intervals[i]);
+            }
             i++;
         }
 
-        return list.toArray(new int[list.size()][]);
+        return list.toArray(new int[list.size()][]);        
+        
     }
 }
