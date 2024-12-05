91311c27a4e48df37b1322cb68eb1e832eb585ab
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-05 14:12:45 +0530

    Time: 12 ms (72.83%) | Memory: 46.7 MB (7.34%) - LeetSync

--- 1094-matrix-cells-in-distance-order/matrix-cells-in-distance-order.java
+++ 1094-matrix-cells-in-distance-order/matrix-cells-in-distance-order.java
@@ -1,21 +1,24 @@
 class Solution {
     public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
-        int[][] ans = new int[rows * cols] [2];
+        // creating an ans 2d array containing the arrays of {i, j}, {i, J}, {i, j};
+        int[][] ans = new int[rows * cols][2];
 
-        int x = 0; 
-        for(int i=0; i< rows; i++){
-            for(int j= 0; j< cols; j++){
+        // filling the coordinates with the rows and cols indices 
+        int x = 0;
+        for(int i = 0; i< rows; i++){
+            for(int j = 0; j< cols; j++){
                 ans[x][0] = i;
-                ans[x][1] = j; 
-                x++; 
+                ans[x][1] = j;
+                x++;
             }
         }
 
-        Arrays.sort(ans, (a,b) ->{
+        Arrays.sort(ans, (a,b) -> {
             int d1 = Math.abs(a[0] - rCenter) + Math.abs(a[1] - cCenter);
             int d2 = Math.abs(b[0] - rCenter) + Math.abs(b[1] - cCenter);
             return d1 -d2;
         });
-        return ans; 
+
+        return ans;
     }
 }
