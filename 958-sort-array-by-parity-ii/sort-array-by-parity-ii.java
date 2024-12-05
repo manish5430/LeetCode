c54438dc280a3dd846f70af5d38cf94c231c42c7
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-05 12:29:38 +0530

    Time: 2 ms (99.78%) | Memory: 46.7 MB (80.74%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.java
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.java
@@ -1,32 +1,21 @@
 class Solution {
     public int[] sortArrayByParityII(int[] nums) {
-        ArrayList<Integer> list = new ArrayList<>();
-
-        for(int a = 0; a<nums.length; a++){
-            list.add(null);
-        }
-
-        int i =0; 
+        int[] ans = new int[nums.length];
+       
+        int i = 0;
+        int j = 0;
         for(int n : nums){
-            if(n%2 == 0){
-                list.set(i, n);
+            if(n% 2 == 0){
+                ans[i] = n;
                 i += 2;
             }
-        }
-        int k = 0;
-        for(int n: nums){
-            if(n%2 != 0){
-                list.set(k*2 +1, n);
-                k++;
+            else{
+                ans[j*2 +1] = n;
+                j++;
             }
-        }       
-
-
-        int[] asn = new int[list.size()];
-        for(int j= 0; j< asn.length; j++){
-            asn[j] = list.get(j);
         }
 
-        return asn;
+        return ans;        
+       
     }
 }
