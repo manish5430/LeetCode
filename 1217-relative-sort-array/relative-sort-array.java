ee1e103e50764d03335f34a25ba96d7dfb1d13ac
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-09 14:56:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (84.79%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -1,33 +1,28 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        int max = 0;
-        for(int num : arr1){
-            max = Math.max(num, max);
-        }
-
-        int[] count = new int[max +1];
-        for(int num : arr1){
-            count[num]++;
-        }
-        
+        int[] freqArr = new int[1001];
         int[] ans = new int[arr1.length];
-        int index = 0; 
+
+        for(int num : arr1){
+            freqArr[num]++;
+        }
+
+        int index = 0;
         for(int num : arr2){
-            while(count[num] > 0){
-                ans[index] = num;
-                count[num]--;
-                index++;
+            while(freqArr[num] > 0){
+                ans[index++] = num;
+                freqArr[num]--;
             }
         }
 
-        for(int i = 0; i< count.length; i++){
-            while(count[i]> 0){
-                ans[index] = i;
-                count[i]--;
-                index++;
+        for(int num = 0; num < freqArr.length; num++){
+            while(freqArr[num] > 0){
+                ans[index++] = num;
+                freqArr[num]--;
             }
         }
-        return ans;
-    
+
+        return ans;       
+        
     }
 }
