3043ebd8ab247f0d58bdaabdf70585cb3a302a9f
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-11 16:11:21 +0530

    Time: 15 ms (46.24%) | Memory: 45.7 MB (40.83%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
@@ -13,21 +13,27 @@ class Solution {
             }
 
             if(i < n && j < n){
+
+                // after removing all the _ the elemenet must match 
                 if(start.charAt(i) != target.charAt(j)){
                     return false;
                 }
-                if(target.charAt(j) == 'L'  &&  i < j){
+
+                // if the element matches but the posistion is different 
+                if(target.charAt(j) == 'L'  &&  i < j){  // i cant move forward for L
                     return false;
                 }
-                if(target.charAt(j) == 'R'  &&  i > j){
-                    return false;
+                if(target.charAt(j) == 'R'  &&  i > j){  // i cant move backward for R
+                    return false;  
                 }
 
                 i++;
                 j++;
             }
         }
-
+        
+        //  after all the L R s if anything left just return false;
+        
         while(i< n){
             if(start.charAt(i) != '_'){
                 return false;
