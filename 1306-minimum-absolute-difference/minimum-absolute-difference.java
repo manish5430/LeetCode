0e1dc56670a77838133beb74ecc06e02429cc80f
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-11 12:11:58 +0530

    Time: 20 ms (19.93%) | Memory: 57.2 MB (16.09%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.java
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.java
@@ -1,29 +1,20 @@
 class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
-        List<List<Integer>> ans = new ArrayList<>();
-        int min = Integer.MAX_VALUE;
-
+        ArrayList<List<Integer>> list = new ArrayList<>();
         Arrays.sort(arr);
 
-        for(int i =0; i< arr.length -1; i++){
-            int min2 = ( arr[i+1] - arr[i] );
-                    
-                if(min2 < min){
-                    min = min2;
-                    ans.clear();
-                    
-                    List<Integer> pair = new ArrayList<>();
-                    pair.add(arr[i]);
-                    pair.add(arr[i+1]);
-                    ans.add(pair);
-                }
-                else if (min == min2){
-                    List<Integer> pair = new ArrayList<>();
-                    pair.add(arr[i]);
-                    pair.add(arr[i+1]);
-                    ans.add(pair);
-                }
+        int min = arr[1] - arr[0];        
+
+        for(int i = 1; i< arr.length; i++){
+            min = Math.min(arr[i] - arr[i-1], min);            
         }
-        return ans;
+
+        for(int i = 1; i< arr.length; i++){
+            if(arr[i] - arr[i-1] == min){
+                List<Integer> sublist = List.of(arr[i-1], arr[i]);
+                list.add(sublist);
+            }
+        }
+        return list;
     }
 }
