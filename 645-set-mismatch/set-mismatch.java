5639076d0482173ea5cbf93dcc88478de2d975b4
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-12 19:43:37 +0530

    Time: 12 ms (41.49%) | Memory: 46.1 MB (11.05%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,32 +1,25 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        ArrayList<Integer> list = new ArrayList<>();
-        Arrays.sort(nums);
+        HashSet<Integer> set = new HashSet<>();
+        int[] freqArr = new int[10001];
+        int[] ans = new int[2];
 
-        for(int i= 0; i< nums.length;){
-            int index = nums[i] -1;
-            if(nums[i] != nums[index]){
-                swap(nums, i, index);
+        for(int num : nums){
+            set.add(num);
+            freqArr[num]++;
+        }       
+
+        for(int num : nums){
+            if(freqArr[num] == 2){
+                ans[0] = num;
             }
-            else{
-                i++;
+        }       
+
+        for(int i = 1; i<= nums.length; i++){
+            if(!set.contains(i)){
+                ans[1] = i;
             }
-        }
-        for(int i=0; i< nums.length; i++){
-            if( nums[i] != (i +1)){
-                list.add(nums[i]);
-                list.add(i+1);
-            }
-        }
-        int[] ans = new int[list.size()];
-        for(int i=0; i< ans.length; i++){
-            ans[i] = list.get(i);
         }
         return ans;
-    }
-    static void swap(int[] nums, int start,int end){
-        int temp = nums[start]; 
-        nums[start] = nums[end];
-        nums[end] = temp;
     }
 }
