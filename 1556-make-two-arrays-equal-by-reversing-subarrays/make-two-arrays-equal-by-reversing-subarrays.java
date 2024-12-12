2aa35ff94254dba0c7c0c08e6f78874db1d08faa
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-12 17:24:25 +0530

    Time: 4 ms (64.01%) | Memory: 44.4 MB (36.10%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,19 +1,8 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        ArrayList<Integer> list = new ArrayList<>();
+        Arrays.sort(target);
+        Arrays.sort(arr);
 
-        for(int num : arr){
-            list.add(num);
-        }
-
-        for(int num : target ){
-            if(list.contains(num)){ 
-                list.remove(Integer.valueOf(num));
-            }
-            else{
-                return false;
-            }
-        }
-        return list.isEmpty();        
+        return Arrays.equals(target, arr);
     }
 }
