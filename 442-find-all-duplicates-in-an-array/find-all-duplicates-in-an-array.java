b1761ac5525744195513821597600abf9bc76f4c
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-13 20:11:03 +0530

    Time: 6 ms (75.23%) | Memory: 55.9 MB (7.86%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -8,6 +8,8 @@ class Solution {
 
         // make all elements negative at first one by one 
         // then check if at that correct index the element is negative 
+
+        // iterate over nums and make each element negative now if we iterate over nums if we find any negative element means we have iterated over this before means its duplicate so just add it to list 
             if(nums[index] > 0){
                 nums[index] = - nums[index];
             }else{
