4cf6ba8afc4298518e6a8281b932d2165fc40768
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-16 12:02:02 +0530

    Time: 16 ms (71.99%) | Memory: 45.1 MB (42.78%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -7,49 +7,33 @@ class Solution {
         int end = 0;
 
         for(int i = 0; i< s.length()-1; i++){
-            int left = i;
-            int right = i;
             
-            while(left >= 0 && right < s.length()){
-                if(s.charAt(left) == s.charAt(right)){
-                    left--;
-                    right++;
-                }
-                else{
-                    break;
-                }
+            int[] odd = expand(s, i, i);
+            int[] even = expand(s, i, i+1);
+
+            if(odd[1] - odd[0] -1   > max){
+                max = odd[1] - odd[0] -1;
+                start = odd[0] + 1;
+                end = odd[1] -1;
             }
 
-            int length = right - left -1; 
-            if(length > max){
-                max = length;
-                start = left + 1;
-                end = right -1;
+            if(even[1] - even[0] -1   > max){
+                max = even[1] - even[0] -1;
+                start = even[0] + 1;
+                end = even[1] -1;
             }
-        }
-
-        for(int i = 0; i< s.length()-1; i++){
-            int left = i;
-            int right = i +1 ;   // for even length string 
-
-            while( left >= 0  && right< s.length()){
-                if(s.charAt(left) == s.charAt(right)){
-                    left--;
-                    right++;
-                }
-                else{
-                    break;
-                }
-            }
-
-            int length = right - left -1;
-            if(length > max){
-                max = length;
-                start = left +1; 
-                end = right -1;
-            }
-        }
+        }       
 
         return s.substring(start, end +1);
+    }
+
+    static int[] expand(String s, int left, int right){
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+
+        return new int[]{left, right};
+
     }
 }
