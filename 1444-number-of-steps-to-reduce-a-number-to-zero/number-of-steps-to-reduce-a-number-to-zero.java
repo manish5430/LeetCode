6bcc1ea527c5ac0b06d49018751e7906551e6870
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-18 22:03:43 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (38.56%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,15 +1,13 @@
 class Solution {
     public int numberOfSteps(int num) {
-        return steps(num, 0);
-        
-    }
-    static int steps(int num, int count){
-        if(num == 0) return count;
 
-        if(num%2 == 0){
-            return steps(num/2, count+1);
-        }else{
-            return steps(num-1, count+1);
+        if(num == 0) return 0;  // here it returns 0 because i need to just end it here 
+        // and it only returns 0 at the end of stack when the recurive call starts backing up
+        // they will keep +1 and returning other calls 
+
+        if(num %2 == 0){
+            return 1 + numberOfSteps(num /2);
         }
+        return 1 + numberOfSteps(num -1);
     }
 }
