e511c540f4f06914d14ec5fb2bc4fd1d6ce9078b
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-19 12:49:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (25.24%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
@@ -3,20 +3,16 @@ class Solution {
         int[] sort = nums.clone();
         Arrays.sort(sort);
 
-        int i = 0; 
         int count = 0;
-        while(i< nums.length){
 
-            int sum = 0;
-            int sum2 = 0;
-            for(int j = i; j< nums.length; j++){
-                sum += nums[j];
-                sum2 += sort[j];                
+        int sum = 0;
+        int sum2 = 0;
+        for(int i = 0; i< nums.length; i++){
+            sum += nums[i];
+            sum2 += sort[i];                
 
-                if(sum == sum2){
-                    count++;
-                    i = j +1;               
-                }
+            if(sum == sum2){
+                count++;               
             }
         }
         return count;
