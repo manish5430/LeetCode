8ce67cf3d086ddae16605c59070e7d4d0fd617ee
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-22 16:41:13 +0530

    Time: 13 ms (56.63%) | Memory: 45 MB (52.47%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,24 +1,36 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-         List<Integer> result = new ArrayList<>();
-        if (s.length() < p.length()) return result;
-        int[] pCount = new int[26];
-        int[] sCount = new int[26];
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        if(s.length() < p.length()) return list;
+
+        for(int i = 0; i< p.length(); i++){
+            freq1[p.charAt(i) - 'a']++;
+            freq2[s.charAt(i) - 'a']++;
+        }         
+        if(isMatch(freq1, freq2))  list.add(0);
+
+        for(int i = 1; i<= s.length() - p.length(); i++){     
+            freq2[s.charAt(i-1) - 'a']--;
+            freq2[s.charAt(i + p.length() -1) - 'a']++;
         
-        for (char c : p.toCharArray()) {
-            pCount[c - 'a']++;
-        }
-        for (int i = 0; i < s.length(); i++) {
-            sCount[s.charAt(i) - 'a']++;
-            if (i >= p.length()) {
-                sCount[s.charAt(i - p.length()) - 'a']--;
-            }
-
-            if (Arrays.equals(sCount, pCount)) {
-                result.add(i - p.length() + 1);
+            if(isMatch(freq1, freq2)){
+                list.add(i);
             }
         }
+        return list;
+    }
 
-        return result;
+    static boolean isMatch(int[] freq1, int[] freq2){
+ 
+        for(int i = 0; i< 26; i++){
+            if(freq1[i] != freq2[i]){
+                return false;
+            }
+        }
+        return true;
     }
 }
