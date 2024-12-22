02510b54822520342e5481ac8d6c8269f5919423
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-23 00:31:09 +0530

    Time: 1602 ms (9.05%) | Memory: 42.2 MB (83.18%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,39 +1,28 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if(s.length() <= 1)  return s;
-
-        int max = 0;
-        int start = 0;
-        int end = 0;
-
-        for(int i = 0; i< s.length()-1; i++){
-            
-            int[] odd = expand(s, i, i);
-            int[] even = expand(s, i, i+1);
-
-            if(odd[1] - odd[0] -1   > max){
-                max = odd[1] - odd[0] -1;
-                start = odd[0] + 1;
-                end = odd[1] -1;
+        int max = 0; int start = 0; int end = 0; // to store the max substring indices 
+        for(int i = 0; i< s.length(); i++){
+            for(int j = 0; j< s.length(); j++){
+                if(isPalindrome(s, i, j)){
+                    if((j-i +1) > max){
+                        max = j-i+1;
+                        start = i;
+                        end = j;
+                    }
+                }
             }
-
-            if(even[1] - even[0] -1   > max){
-                max = even[1] - even[0] -1;
-                start = even[0] + 1;
-                end = even[1] -1;
-            }
-        }       
-
-        return s.substring(start, end +1);
+        }
+        return s.substring(start, end+1);
+        
     }
 
-    static int[] expand(String s, int left, int right){
-        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-            left--;
-            right++;
+    static boolean isPalindrome(String s, int i , int j){
+        while(i< j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            } 
+            i++; j--;
         }
-
-        return new int[]{left, right};
-
+        return true;
     }
 }
