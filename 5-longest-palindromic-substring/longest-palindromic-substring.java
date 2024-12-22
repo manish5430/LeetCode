df0fb0f9bee9a34289bcdfe2bfdf47b84413ac1f
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-23 00:57:32 +0530

    Time: 18 ms (64.81%) | Memory: 42 MB (93.55%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,28 +1,51 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int max = 0; int start = 0; int end = 0; // to store the max substring indices 
-        for(int i = 0; i< s.length(); i++){
-            for(int j = 0; j< s.length(); j++){
-                if(isPalindrome(s, i, j)){
-                    if((j-i +1) > max){
-                        max = j-i+1;
-                        start = i;
-                        end = j;
-                    }
+        int max = 0; int start = 0; int end = 0;
+
+        // odd length string 
+        for(int i = 0; i< s.length()-1; i++){
+            int left = i; 
+            int right = i;
+
+            while(left >= 0 && right < s.length()){
+                if(s.charAt(left) == s.charAt(right)){
+                    left--;
+                    right++;
+                }else{        // move to next i  beacuse it break in between 
+                    break;    // not so long palindrome it break here so store it length now
                 }
             }
-        }
-        return s.substring(start, end+1);
-        
-    }
 
-    static boolean isPalindrome(String s, int i , int j){
-        while(i< j){
-            if(s.charAt(i) != s.charAt(j)){
-                return false;
-            } 
-            i++; j--;
+            int length = right - left +-1;
+            if(length > max){
+                max = length; 
+                start = left +1;
+                end = right -1;
+            }
         }
-        return true;
+
+        // now for even length string 
+        for(int i = 0; i< s.length(); i++){
+            int left = i;
+            int right = i+1;
+
+            while(left >= 0 && right < s.length()){
+                if(s.charAt(left) == s.charAt(right)){
+                    left--;
+                    right++;
+                }else{
+                    break;
+                }
+            }
+
+            int length = right - left -1;
+            if(length > max){
+                max = length;
+                start = left +1;
+                end = right -1;
+            }
+        }
+
+        return s.substring(start, end+1);
     }
 }
