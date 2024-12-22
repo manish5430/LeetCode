376f3b2d18dc2611ea3e9f2644a8f7d8dcc5fa58
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-22 14:57:56 +0530

    Time: 84 ms (16.47%) | Memory: 45.4 MB (7.42%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,32 +1,30 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) return false;
+        int[] freqArr = new int[26];
 
-        int[] s1Count = new int[26];
-        int[] s2Count = new int[26];
-
-        // Count the frequency of characters in s1 and the first window of s2
-        for (int i = 0; i < s1.length(); i++) {
-            s1Count[s1.charAt(i) - 'a']++;
-            s2Count[s2.charAt(i) - 'a']++;
+        for(char ch : s1.toCharArray()){
+            freqArr[ch - 'a']++;
         }
 
-        // Slide the window over s2
-        for (int i = 0; i < s2.length() - s1.length(); i++) {
-            if (matches(s1Count, s2Count)) return true;
-            // Update the window
-            s2Count[s2.charAt(i) - 'a']--;
-            s2Count[s2.charAt(i + s1.length()) - 'a']++;
-        }
+        for(int i = 0; i<= s2.length()- s1.length(); i++){
+            String s = s2.substring(i, i + s1.length());
 
-        // Check the last window
-        return matches(s1Count, s2Count);
-    }
+            int[] freq2 = new int[26];
+            for(char c : s.toCharArray()){
+                freq2[c - 'a']++;
+            }
 
-    private boolean matches(int[] s1Count, int[] s2Count) {
-        for (int i = 0; i < 26; i++) {
-            if (s1Count[i] != s2Count[i]) return false;
+            boolean match = true;
+            for(int j = 0; j< 26; j++){
+                if(freq2[j] != freqArr[j]){
+                    match = false;
+                    break;
+                }
+            }
+
+            if(match) return true;
+
         }
-        return true;
+        return false;  
     }
 }
