be965fb03ba12ad4d3455a98028176126d7bc977
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-23 15:11:46 +0530

    Time: 2 ms (49.11%) | Memory: 41.1 MB (21.63%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -15,8 +15,9 @@ class Solution {
             return list.get(0);
         }
 
+    // kth value is counted from 0th index 0 1 2 3 k but we haeve ot remove kth number as 1st indexed so start = 0 + k -1 
         start = (start + k-1) % list.size();
-        list.remove(start);
+        list.remove(start);  // we removed the element at start index now so new num will be at strat index now and same process as start + k -1 but dont forget the modulo we have to roll back too
 
         return recursion(list, n-1, k, start);
     }
