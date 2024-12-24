2a8e9fa9c06fb79ca38394ca9e70ee1b39c7018c
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-24 23:20:48 +0530

    Time: 796 ms (7.53%) | Memory: 45.6 MB (7.56%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,34 +1,18 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int[] freqArr = new int[26];
-        int[] freq2 = new int[26]; 
+        char[] s1arr = s1.toCharArray();
+        Arrays.sort(s1arr);
+        String s1str = new String(s1arr);
 
-        if(s1.length() > s2.length()) return false;
+        for(int i = 0; i<= s2.length()-s1.length(); i++){
+            String substring = s2.substring(i, i+s1.length());
+            char[] s2arr = substring.toCharArray();
+            Arrays.sort(s2arr);
+            String s2str = new String(s2arr);
 
-        for(int i = 0; i< s1.length(); i++){
-            freqArr[s1.charAt(i) - 'a']++;
-            freq2[s2.charAt(i) - 'a']++;  // for initial window just that 
-        }
-
-        if(isMatch(freqArr, freq2)) return true;
-
-        for(int i = 1; i<= s2.length()- s1.length(); i++){
-
-            freq2[s2.charAt(i-1) - 'a']--;   // last one dcrement 
-            freq2[s2.charAt(i + s1.length() -1) - 'a']++;  // +1 char increament 
-
-            if(isMatch(freqArr, freq2)) return true;
+            if(s1str.equals(s2str)) return true;
         }
         return false;
-           
-    }
-
-    static boolean isMatch(int[] freqArr, int[] freq2){
-        for(int i = 0; i<26; i++){
-            if(freqArr[i] != freq2[i]){
-                return false;
-            }
-        }
-        return true;
+        
     }
 }
