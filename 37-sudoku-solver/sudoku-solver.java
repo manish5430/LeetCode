69624a7f270524ad3bad70cdc1f8214725d9f8ee
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-26 19:58:38 +0530

    Time: 254 ms (26.26%) | Memory: 41.2 MB (35.85%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,106 +1,68 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        int[][] sudoku = new int[board.length][board[0].length];
 
-        for(int i = 0; i< board.length; i++){
-            for(int j = 0; j< board.length; j++){
-                if(board[i][j] == '.'){
-                    sudoku[i][j] = 0;
-                }else{
-                    sudoku[i][j] = board[i][j] - '0';
-                }
-            }
-        }
-        if(backtracking(sudoku)){
-            // update teh board array itself from solved sudoku[][]
-            for(int i = 0; i< board.length; i++){
-                for(int j = 0; j< board.length; j++){
-                    board[i][j] = (char)(sudoku[i][j] + '0');
-                }
-            }
-        }
-        display(board);
+        backtracking(board);
+        System.out.println(Arrays.deepToString(board));
+        
     }
 
-    static boolean backtracking(int[][] board){
+    static boolean backtracking(char[][] board){
         int n = board.length;
         int row = -1;
         int col = -1;
 
-        boolean isEmptyLeft = false;
+        boolean isEmpty = false;
         for(int i = 0; i< n; i++){
             for(int j = 0; j< n; j++){
-                if(board[i][j] == 0){
+                if(board[i][j] == '.'){
                     row = i;
                     col = j;
-                    isEmptyLeft = true;
+                    isEmpty = true;
                     break;
                 }
             }
-            if(isEmptyLeft == true){
-                break;  // move to the next row 
-            }
+            if(isEmpty == true)  break;
         }
-        if(isEmptyLeft == false){
-            return true;  // evryone is placed
-        }
+        if(isEmpty == false)  return true;  // meaning end of empty cells 
 
-        for(int number = 1; number<= 9; number++){
-            if(isSafe(board, row, col, number)){
-                board[row][col] = number;
-            
-// this is to find the next empty cell and if we find the next empty cell then then
-// the isemptyleft will be true and it will not retunr true meaning it will keep fiinding 
-// the next empty cell untill all the eclls are filled
+        for(int num = 1; num<= 9; num++){
+            if(isSafe(board, row, col, num)){
+                board[row][col] = (char)(num + '0');
+
                 if(backtracking(board)){
-                    return true; 
-                    // here it wont even come to true untill finding all the empty cells 
+                    return true;
                 }else{
-                    board[row][col] = 0; 
-                    // simple backtracking meaning sudoku is not solved even after filling all 
-                    // cell so backtrack 
+                    board[row][col] = '.';
                 }
             }
         }
-        return false;   // cannnot solve sudoku 
+        return false;
     }
 
-    static boolean isSafe(int[][] board, int row, int col, int number){
-        // checking every col of a row
+    static boolean isSafe(char[][] board, int row, int col, int num){
         for(int i = 0; i< 9; i++){
-            if(board[row][i] == number){
+            if(board[row][i] == (char)(num + '0')){
                 return false;
             }
         }
 
-        // checking in one particular column for every row 
         for(int i = 0; i< 9; i++){
-            if(board[i][col] == number){
+            if(board[i][col] == (char)(num + '0')){
                 return false;
             }
         }
 
-        // checking for every 3 x 3 grid 
-        int sqrt = (int)Math.sqrt(board.length);
+        int sqrt = (int)(Math.sqrt(board.length));
         int rowStart = row - row % sqrt;
         int colStart = col - col % sqrt;
 
-        for(int i = rowStart; i< rowStart + sqrt; i++){
-            for(int j = colStart; j< colStart + sqrt; j++){
-                if(board[i][j] == number){
+        for(int r = rowStart; r < rowStart + sqrt; r++){
+            for(int c = colStart; c < colStart + sqrt; c++){
+                if(board[r][c] == (char)(num + '0')){
                     return false;
                 }
             }
         }
         return true;
     }
-    static void display(char[][] board) {
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[i].length; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
 }
