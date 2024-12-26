c8179354bf0e10e8ffb733a073604ff6e0bac53e
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-26 15:27:08 +0530

    Time: 265 ms (25.78%) | Memory: 41.5 MB (14.82%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -19,6 +19,7 @@ class Solution {
                 }
             }
         }
+        display(board);
     }
 
     static boolean backtracking(int[][] board){
@@ -92,6 +93,14 @@ class Solution {
             }
         }
         return true;
+    }
+    static void display(char[][] board) {
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
 }
