88ed141b2a54bacc50794ee200e98f736d591952
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2024-12-28 16:15:58 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (66.78%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,12 +1,19 @@
 class Solution {
-    int mod = (int)(1e9 +7);
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
+
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
+
         int[][] dp = new int[m][n];
+        return recursion(obstacleGrid, dp);
 
-        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) return 0;
+    }
 
+    static int recursion(int[][] obstacleGrid, int[][] dp){
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        
         for(int i = 0; i< m; i++){
             for(int j = 0; j< n; j++){
 
@@ -14,15 +21,13 @@ class Solution {
                 else if(i == 0 && j == 0) dp[i][j] = 1;
                 else{
                     int down = 0, right = 0;
-                    if(i> 0) down = dp[i-1][j];
-                    if(j> 0) right = dp[i][j-1];
-                    
-                    dp[i][j] = (down + right);
-                }
+                    if(i > 0) down = dp[i-1][j];
+                    if(j > 0) right = dp[i][j-1];
 
+                    dp[i][j] = down + right;
+                }
             }
         }
-       
-       return dp[m-1][n-1];
+        return dp[m-1][n-1];
     }
 }
