c5ed78515c20b4c3cc73ddea3aaf358a278e14b2
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-02 23:20:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (34.51%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        ArrayList<List<Integer>> result = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        recursion(k, 1, n, result, list);
+        return result;
+    }
+
+    static void recursion(int k, int index, int target, ArrayList<List<Integer>> result, 
+                            ArrayList<Integer> list){
+
+        if(list.size() >= k){
+            if(target == 0){
+                result.add(new ArrayList<>(list));
+            }
+            return;
+        }
+
+        for(int i = index; i<= 9; i++){
+            if(i <= target){
+                list.add(i);
+                recursion(k, i+1, target - i, result, list);
+                list.remove(list.size() -1);
+            }
+        }
+
+    }
+}
