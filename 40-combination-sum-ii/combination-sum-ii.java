9857408d9a2dc71a5194c8e833e120698f25ac85
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-02 19:34:38 +0530

    Time: 3 ms (77.68%) | Memory: 42.9 MB (96.43%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] nums, int target) {
+        ArrayList<List<Integer>> result = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        Arrays.sort(nums);
+
+        recursion(nums, 0, target, result, list);
+        
+        return result;
+
+    }
+
+    static void recursion(int[] nums, int index, int target, ArrayList<List<Integer>> result,
+                                        ArrayList<Integer> list){
+                                    
+        if(target == 0){
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        else{
+            for(int i = index; i< nums.length; i++){
+                if(i > index && nums[i] == nums[i-1]) continue;
+                if (nums[i] > target) break;
+
+                list.add(nums[i]);
+                recursion(nums, i+1, target - nums[i], result, list);
+                list.remove(list.size() -1);
+            }
+        }
+
+    }
+}
