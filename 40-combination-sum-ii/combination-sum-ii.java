e5359753760d52fabe7246d876be220728641197
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-03 16:51:56 +0530

    Time: 2 ms (99.77%) | Memory: 43.2 MB (73.53%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -5,28 +5,28 @@ class Solution {
         Arrays.sort(nums);
 
         recursion(nums, 0, target, result, list);
-        
-        return result;
-
+        return result;        
     }
 
     static void recursion(int[] nums, int index, int target, ArrayList<List<Integer>> result,
-                                        ArrayList<Integer> list){
-                                    
+                                ArrayList<Integer> list){
         if(target == 0){
             result.add(new ArrayList<>(list));
             return;
         }
         else{
-            for(int i = index; i< nums.length; i++){
-                if(i > index && nums[i] == nums[i-1]) continue;
-                if (nums[i] > target) break;
-
+            for(int i= index; i< nums.length; i++){
+                if(i > index && nums[i] == nums[i -1]) continue;
+                if(nums[i] > target)  break;
+                    
                 list.add(nums[i]);
-                recursion(nums, i+1, target - nums[i], result, list);
+                recursion(nums, i +1, target - nums[i], result, list);
                 list.remove(list.size() -1);
+                
             }
         }
+
+
 
     }
 }
