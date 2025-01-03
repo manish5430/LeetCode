2e8d3e8223634f35943c0f674931657896d6b4e5
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-03 17:51:58 +0530

    Time: 3 ms (59.69%) | Memory: 58.6 MB (55.40%) - LeetSync

---
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+        long left = 0;
+        long right = 0;
+        
+        for(int num : nums){
+            right += num;
+        }
+
+        int count = 0;
+        for(int i = 0; i< nums.length-1; i++){
+            left += nums[i];
+            right -= nums[i];
+
+            if(left >= right) count++;
+        }
+        
+        return count;
+    }
+}
