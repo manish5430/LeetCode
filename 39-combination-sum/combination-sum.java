222e502a45e6c5c8b9e6b02935e6bc0e15f0d8af
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-03 16:35:20 +0530

    Time: 2 ms (83.50%) | Memory: 45.1 MB (8.19%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,30 +1,28 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] nums, int target) {
-        ArrayList<List<Integer>> ans = new ArrayList<>();
+        ArrayList<List<Integer>> result = new ArrayList<>();
         ArrayList<Integer> list = new ArrayList<>();
 
-        recursion(nums, 0, target, ans, list);
-        return ans;        
+        recursion(nums, 0, target, result, list);
+        return result;
     }
 
-    static void recursion(int[] nums, int i, int target, ArrayList<List<Integer>> ans,
-                            ArrayList<Integer> list){
-        if(i == nums.length || target < 0){
+    static void recursion(int[] nums, int i, int target, ArrayList<List<Integer>> result,
+                                ArrayList<Integer> list){
+        if(target < 0 || i == nums.length){
             return;
         }
 
         if(target == 0){
-            ans.add(new ArrayList<>(list));
+            result.add(new ArrayList<>(list));
             return;
         }
 
         list.add(nums[i]);
-        recursion(nums, i, target - nums[i], ans, list);
-        list.remove(list.size() -1);    // backtracking 
+        recursion(nums, i, target - nums[i], result, list);
+        list.remove(list.size() -1);
 
-        // see whats happening here we are reusing the same number again but we want to use each no once standard case we can just simply skip and consider what to do with the next number so these two recursive calls are sufficient
-
-        recursion(nums, i+1, target, ans, list);            
-        
+        recursion(nums, i+1, target, result, list);
+    
     }
 }
