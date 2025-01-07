134cd8877b07367412c0c218a300f7eaf98dddeb
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-07 23:12:27 +0530

    Time: 31 ms (33.65%) | Memory: 56.3 MB (52.11%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,39 +1,39 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        // solving with quickSort 
-        quickSort(nums, 0, nums.length-1);
-        return nums;
+        if(nums.length == 1) return nums;
+        int mid = nums.length /2;
 
+        int[] first = sortArray(Arrays.copyOfRange(nums, 0, mid));
+        int[] second = sortArray(Arrays.copyOfRange(nums, mid, nums.length));
+        return mergesort(nums, first, second);
     }
 
-    static void quickSort(int[] nums, int start, int end){
-        if(start >= end) return;
-        
-        int i = start;
-        int j = end;
-        int mid = i + (j -i)/2;
-        int pivot = nums[mid];
+    static int[] mergesort(int[] nums, int[] first, int[] second){
+        int[] result = new int[first.length + second.length];
 
-        while(i <= j){
-            while(nums[i] < pivot){
+        int i = 0, j = 0, k = 0;
+        while(i< first.length && j< second.length){
+            if(first[i] < second[j]){
+                result[k] = first[i];
                 i++;
             }
-            while(nums[j] > pivot){
-                j--;
+            else{
+                result[k] = second[j];
+                j++;
             }
-
-            if(i <= j){
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                i++;
-                j--;
-            }
+            k++;
         }
 
-        quickSort(nums, start, j);
-        quickSort(nums, i, end);
+        while(i< first.length){
+            result[k] = first[i];
+            i++; k++;
+        }
+        while(j< second.length){
+            result[k] = second[j];
+            j++; k++;
+        }
 
-        
+        return result;
     }
+
 }
