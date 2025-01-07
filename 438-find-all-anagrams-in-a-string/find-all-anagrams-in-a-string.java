9ef50c239049a9ee366da19c3e629a903c140873
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-07 18:33:39 +0530

    Time: 12 ms (61.45%) | Memory: 44.9 MB (56.48%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,7 +1,6 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         ArrayList<Integer> list = new ArrayList<>();
-
         int[] freq1 = new int[26];
         int[] freq2 = new int[26];
 
@@ -10,22 +9,20 @@ class Solution {
         for(int i = 0; i< p.length(); i++){
             freq1[p.charAt(i) - 'a']++;
             freq2[s.charAt(i) - 'a']++;
-        }         
-        if(isMatch(freq1, freq2))  list.add(0);
+        }
 
-        for(int i = 1; i<= s.length() - p.length(); i++){     
+        if(isMatch(freq1, freq2)) list.add(0);  // initial window starts at 0th index 
+
+        for(int i = 1; i<= s.length() - p.length(); i++){
             freq2[s.charAt(i-1) - 'a']--;
             freq2[s.charAt(i + p.length() -1) - 'a']++;
-        
-            if(isMatch(freq1, freq2)){
-                list.add(i);
-            }
+
+            if(isMatch(freq1, freq2))  list.add(i);
         }
         return list;
     }
 
     static boolean isMatch(int[] freq1, int[] freq2){
- 
         for(int i = 0; i< 26; i++){
             if(freq1[i] != freq2[i]){
                 return false;
