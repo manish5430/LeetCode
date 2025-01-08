7475b645d6024bf252000548acc4585abaea95b0
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-08 13:25:04 +0530

    Time: 278 ms (12.18%) | Memory: 58.4 MB (6.73%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,25 +1,26 @@
 class Solution {
     public int[] sortArray(int[] nums) {
         quicksort(nums, 0, nums.length-1);
-        return nums;        
+        return nums;
+        
     }
 
     static void quicksort(int[] nums, int start, int end){
-        if(start >= end) return;
-
+        if(start >= end) return;   // damn important to stop the recursion
         int i = start;
         int j = end;
         int mid = i + (j-i)/2;
         int pivot = nums[mid];
 
         while(i <= j){
-            while(nums[i] < pivot){  // it will stop when it finds any no. not < pivot
+            while(nums[i] < pivot){
                 i++;
             }
-            while(nums[j] > pivot){  // it will stop when it finds any no. not > pivot
+            while(nums[j] > pivot){
                 j--;
             }
-            if(i <= j){    // swpa those two nums[i] and nums[j];
+
+            if(i <= j){
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
@@ -27,8 +28,7 @@ class Solution {
                 j--;
             }
         }
-        quicksort(nums, start, j);  // to the point j has come till now from right end
-        quicksort(nums, i, end);   // from the point i has iterated froom starting 
+        quicksort(nums, start, j);
+        quicksort(nums, i, end);
     }
-
 }
