ecb3d13681b4658b12ed19cd265fe38e12c4b5e9
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-10 00:13:56 +0530

    Time: 10 ms (59.79%) | Memory: 45.3 MB (40.62%) - LeetSync

---
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numTilePossibilities(String tiles) {
+        HashSet<String> set = new HashSet<>();
+        boolean[] visited = new boolean[tiles.length()];
+        char[] arr = tiles.toCharArray();
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        recursion(arr, visited, sb, set);
+        return set.size();
+    }
+
+    static void recursion(char[] arr, boolean[] visited,StringBuilder sb, HashSet<String> set){
+        if(sb.length() > 0){
+            set.add(sb.toString());
+        }
+
+        for(int i = 0; i< arr.length; i++){
+            if(visited[i] || i>0 && arr[i] == arr[i-1] && !visited[i-1]) continue;
+            
+            visited[i] = true;
+            sb.append(arr[i]);
+            recursion(arr, visited, sb, set);
+            sb.deleteCharAt(sb.length() -1);
+            visited[i] = false;
+        }
+    }
+}
