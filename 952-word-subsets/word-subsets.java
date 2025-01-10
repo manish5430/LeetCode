ff69596e98fe169ec4b432be4a90067b80ba57e2
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-11 00:54:03 +0530

    Time: 15 ms (30.23%) | Memory: 54.8 MB (48.84%) - LeetSync

--- 952-word-subsets/word-subsets.java
+++ 952-word-subsets/word-subsets.java
@@ -1,33 +1,35 @@
 class Solution {
     public List<String> wordSubsets(String[] words1, String[] words2) {
-        ArrayList<String> list = new ArrayList<>();
         int[] maxfreq = new int[26];
-        for(String word : words2){
+        ArrayList<String> list = new ArrayList<>();
+        for(String str : words2){
             int[] freq = new int[26];
-            for(char c : word.toCharArray()){
+            for(char c : str.toCharArray()){
                 freq[c - 'a']++;
             }
+
             for(int i = 0; i< 26; i++){
-                maxfreq[i] = Math.max(maxfreq[i], freq[i]);
+                maxfreq[i] = Math.max(freq[i], maxfreq[i]);
             }
         }
-
+        
+        // boolean isUniversal = true;  can not set it true outside the for
+        // cause inside for loop changes made to it will be passed on to the next iteration
+        // it wont be automatically true for next iteration 
         for(String s : words1){
             int[] freq1 = new int[26];
             for(char c : s.toCharArray()){
                 freq1[c -'a']++;
             }
-
-            boolean isMark = true;
+            
+            boolean isUniversal = true;
             for(int i = 0; i< 26; i++){
                 if(freq1[i] < maxfreq[i]){
-                    isMark = false;
+                    isUniversal = false;
                 }
             }
 
-            if(isMark){
-                list.add(s);
-            }
+            if(isUniversal) list.add(s);
         }
         return list;
     }
