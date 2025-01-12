14bba158f1013eb68d4a1db47b2de2fc6b40ce71
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-12 23:19:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (35.01%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.java
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -1,25 +1,23 @@
 class Solution {
     static long Mod = 1_000_000_007;
     public int countGoodNumbers(long n) {
-        long even = (n +1)/2;
-        long odd = n /2;
+        long even = (n+1)/2;
+        long odd  = n/2;
 
-        long num1 = pow(5, even) % Mod;   // 0. 2, 4, 6, 8
-        long num2 = pow(4, odd) % Mod;   // for prime 2, 3, 5, 7
+        long num1 = pow(5, even) % Mod;
+        long num2 = pow(4, odd) % Mod;
 
-        return (int)((num1 * num2)% Mod);    // for permutations multiply    
+        return (int)((num1 * num2) % Mod);
     }
 
     static long pow(int x, long n){
         if(n == 0) return 1;
-
         long temp = pow(x, n/2) % Mod;
 
         if(n %2 == 0){
             return (temp * temp) % Mod;
-        }
-        else{
-            return (x * temp* temp) % Mod;
+        }else{
+            return (x * temp * temp) % Mod;
         }
     }
 }
