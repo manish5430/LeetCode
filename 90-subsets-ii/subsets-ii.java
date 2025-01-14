e73de1af97bab0884a739bd87f0ae7bbf09742a5
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-14 22:26:57 +0530

    Time: 3 ms (21.80%) | Memory: 43.6 MB (41.28%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
-        ArrayList<List<Integer>> list = new ArrayList<>();
+        ArrayList<List<Integer>> result = new ArrayList<>();
         HashSet<List<Integer>> set = new HashSet<>();
-        list.add(new ArrayList<>());
+        result.add(new ArrayList<>());
+        Arrays.sort(nums);
 
         for(int num : nums){
-            int size = list.size();
+            int size = result.size();
             for(int i = 0; i< size; i++){
-                ArrayList<Integer> sublist = new ArrayList<>(list.get(i));
+                ArrayList<Integer> sublist = new ArrayList<>(result.get(i));
                 sublist.add(num);
-                list.add(sublist);
+                result.add(sublist);
             }
         }
-        set.addAll(list);
+        set.addAll(result);
         return new ArrayList<>(set);
     }
 }
