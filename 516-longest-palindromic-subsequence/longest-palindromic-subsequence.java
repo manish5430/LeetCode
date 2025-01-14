a7b04354a7e29cab7324c1abb36e0fd620a05986
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-14 21:28:59 +0530

    Time: 46 ms (52.73%) | Memory: 55.5 MB (27.84%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,30 +1,27 @@
 class Solution {
-    public int longestPalindromeSubseq(String s1) {
-        StringBuilder sb = new StringBuilder(s1);
-        String s2 = sb.reverse().toString();
+    public int longestPalindromeSubseq(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        String s1  = sb.reverse().toString();
 
-        int m = s1.length();
-        int n = s2.length();
-        int[][] dp = new int[m][n];
-
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        int m = s.length();
+        int[][] dp = new int[m][m];
+        for(int[] rows : dp){
+            Arrays.fill(rows, -1);
         }
 
-        return palindrome(s1, s2, m-1, n-1, dp);
-
+        return recursion(s, s1, m-1, m-1, dp);
     }
 
-    static int palindrome(String s1, String s2, int i, int j, int[][] dp){
+    static int recursion(String s1, String s2, int i, int j, int[][] dp){
         if(i< 0 || j< 0) return 0;
-
-        if(dp[i][j] != -1)  return dp[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
 
         if(s1.charAt(i) == s2.charAt(j)){
-            return dp[i][j] = 1 + palindrome(s1, s2, i-1, j-1, dp);
+            return dp[i][j] = 1 + recursion(s1, s2, i-1, j-1, dp);
         }
         else{
-            return dp[i][j] = Math.max(palindrome(s1, s2, i-1, j, dp), palindrome(s1, s2, i, j-1, dp));
+            return dp[i][j] = Math.max(recursion(s1, s2, i-1, j, dp), 
+                                        recursion(s1, s2, i, j-1, dp));
         }
     }
 }
