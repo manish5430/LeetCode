056169f374327fad46a9f9dfa260801028e140ad
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-15 22:05:55 +0530

    Time: 70 ms (55.49%) | Memory: 41 MB (89.30%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -3,35 +3,41 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
+        int[][] dp = new int[m][n];
+        for(int[] rows : dp){
+            Arrays.fill(rows, -1);
+        }
         int max = 0;
-
         for(int i = 0; i< m; i++){
             for(int j = 0; j< n; j++){
                 if(grid[i][j] != 0){
-                    max = Math.max(max, dfs(grid, i, j));
+                    max = Math.max(max, recursion(grid, i, j, dp));
                 }
             }
         }
         return max;
     }
 
-    static int dfs(int[][] grid, int i, int j){
+    static int recursion(int[][] grid, int i, int j, int[][] dp){
         int m = grid.length;
         int n = grid[0].length;
+        
 
-        if(i< 0 || i>= m || j< 0 || j >= n || grid[i][j] == 0) return 0;
+        if(i < 0 || j< 0 || i>= m || j>= n || grid[i][j] == 0) return 0;
 
+        if(dp[i][j] != -1){
+            return 0;
+        }
+        dp[i][j] = 0;
         int temp = grid[i][j];
-        grid[i][j] = 0;  // marked visited 
-
         int max = 0;
-        max = Math.max( 
-            Math.max(dfs(grid, i-1, j), dfs(grid, i+1, j)),
-            Math.max(dfs(grid, i, j-1), dfs(grid, i, j+1))
+        max = Math.max(
+            (Math.max(recursion(grid, i-1, j, dp), recursion(grid, i+1, j, dp))),
+            (Math.max(recursion(grid, i, j-1, dp), recursion(grid, i, j+1, dp)))
         );
 
-        grid[i][j] = temp;  // backtracking 
+        dp[i][j] = -1;
+        return temp + max;        
 
-        return temp + max;
     }
 }
