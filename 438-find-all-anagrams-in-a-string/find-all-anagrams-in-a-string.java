497496e930c5f4f1243265f057f35513966365f3
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-16 15:52:05 +0530

    Time: 12 ms (61.73%) | Memory: 44.9 MB (67.72%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,30 +1,28 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         ArrayList<Integer> list = new ArrayList<>();
+        int[] freq = new int[26];
         int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
-
-        if(s.length() < p.length()) return list;
+        if(s.length() < p.length()) return new ArrayList<>();
 
         for(int i = 0; i< p.length(); i++){
-            freq1[p.charAt(i) - 'a']++;
-            freq2[s.charAt(i) - 'a']++;
+            freq[p.charAt(i) - 'a']++;
+            freq1[s.charAt(i) - 'a']++;
         }
-
-        if(isMatch(freq1, freq2)) list.add(0);  // initial window starts at 0th index 
+        if(isMatch(freq, freq1)) list.add(0);
 
         for(int i = 1; i<= s.length() - p.length(); i++){
-            freq2[s.charAt(i-1) - 'a']--;
-            freq2[s.charAt(i + p.length() -1) - 'a']++;
+            freq1[s.charAt(i-1) - 'a']--;
+            freq1[s.charAt(i + p.length() -1) - 'a']++;
 
-            if(isMatch(freq1, freq2))  list.add(i);
+            if(isMatch(freq, freq1)) list.add(i);
         }
         return list;
     }
 
-    static boolean isMatch(int[] freq1, int[] freq2){
-        for(int i = 0; i< 26; i++){
-            if(freq1[i] != freq2[i]){
+    static boolean isMatch(int[] freq, int[] freq1){
+        for(int i= 0; i< 26; i++){
+            if(freq[i] != freq1[i]){
                 return false;
             }
         }
