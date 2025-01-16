2165dc1f3db63c381c9be717b0a26952a30e5def
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-16 17:37:09 +0530

    Time: 4 ms (29.82%) | Memory: 45.3 MB (21.75%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -2,29 +2,34 @@ class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         ArrayList<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-        boolean[] visited = new boolean[nums.length];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) +1);
+        }
 
-        backtracking(nums, visited, list, new ArrayList<Integer>());
+        backtracking(nums, map, list, new ArrayList<Integer>());
         return list;
     }
 
-    static void backtracking(int[] nums, boolean[] visited, ArrayList<List<Integer>> result, 
-                                                        ArrayList<Integer> list){
+    static void backtracking(int[] nums, HashMap<Integer, Integer> map,
+                      ArrayList<List<Integer>> result, ArrayList<Integer> list){
+
         if(list.size() == nums.length){
             result.add(new ArrayList<>(list));
             return;
         }
+         
+        for(int num : map.keySet()){
+            if(map.getOrDefault(num, 0) == 0 ){
+                continue;
+            }
 
-        for(int i = 0; i< nums.length; i++){
-            if(visited[i]) continue;
-
-            if(i> 0 && nums[i] == nums[i-1] && !visited[i-1]) continue;
+            list.add(num);
+            map.put(num, map.getOrDefault(num, 0) -1);
+            backtracking(nums, map, result, list);
             
-            visited[i] = true;
-            list.add(nums[i]);   
-            backtracking(nums, visited, result, list);
-            list.remove(list.size() -1);  
-            visited[i] = false;
+            list.remove(list.size() -1);
+            map.put(num, map.getOrDefault(num, 0) +1);
         }
     }
 }
