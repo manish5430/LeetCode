ec35be56ae80c4f1a267ab7bdf43d92905fa5d57
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-16 17:08:10 +0530

    Time: 1 ms (99.85%) | Memory: 44.7 MB (89.22%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,28 +1,30 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        ArrayList<List<Integer>> result = new ArrayList<>();
+        ArrayList<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-        boolean[] isVisited = new boolean[nums.length];
-        recursion(nums, isVisited, new ArrayList<>(), result);
-        return result;        
+        boolean[] visited = new boolean[nums.length];
+
+        backtracking(nums, visited, list, new ArrayList<Integer>());
+        return list;
     }
 
-    static void recursion(int[] nums, boolean[] isVisited, ArrayList<Integer> list,
-                                                        ArrayList<List<Integer>> result){
+    static void backtracking(int[] nums, boolean[] visited, ArrayList<List<Integer>> result, 
+                                                        ArrayList<Integer> list){
         if(list.size() == nums.length){
             result.add(new ArrayList<>(list));
             return;
         }
 
         for(int i = 0; i< nums.length; i++){
-            if(isVisited[i]) continue;
-            if(i> 0 && nums[i] == nums[i-1] && !(isVisited[i-1])) continue;
+            if(visited[i]) continue;
 
-            isVisited[i] = true;
-            list.add(nums[i]);
-            recursion(nums, isVisited, list, result);
-            list.remove(list.size() -1);
-            isVisited[i] = false;
+            if(i> 0 && nums[i] == nums[i-1] && !visited[i-1]) continue;
+            
+            visited[i] = true;
+            list.add(nums[i]);   
+            backtracking(nums, visited, result, list);
+            list.remove(list.size() -1);  
+            visited[i] = false;
         }
     }
 }
