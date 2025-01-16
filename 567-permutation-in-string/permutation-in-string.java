df1fb72a2f20415e67f81c1309e25955831e3c14
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-16 15:42:53 +0530

    Time: 7 ms (71.29%) | Memory: 42.7 MB (75.59%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,27 +1,31 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int[] freq = new int[26];
-        for(char c : s1.toCharArray()){
-            freq[c - 'a']++;
+        int[] freq1 = new int[26];
+        if(s1.length() > s2.length()) return false;
+
+        for(int i = 0; i< s1.length(); i++){
+            freq[s1.charAt(i) - 'a']++;
+            freq1[s2.charAt(i) - 'a']++;
         }
 
-        for(int i = 0; i<= s2.length() - s1.length(); i++){
-            String s = s2.substring(i, i+ s1.length());
-            int[] freq1 = new int[26];
-            
-            for(char c : s.toCharArray()){
-                freq1[c - 'a']++;
-            }
+        if(isMatch(freq, freq1)) return true;
 
-            boolean isMatch = true;
-            for(int j = 0; j< 26; j++){
-                if(freq[j] != freq1[j]){
-                    isMatch = false;
-                }
-            }
-            if(isMatch) return true;     // if there is even one permutation present
+        for(int i = 1; i<= s2.length() - s1.length(); i++){
+            freq1[s2.charAt(i-1) - 'a']--;
+            freq1[s2.charAt(i + s1.length() -1) - 'a']++;
+
+            if(isMatch(freq, freq1)) return true;        
         }
-        return false;
-        
+        return false;        
+    }
+
+    static boolean isMatch(int[] freq, int[] freq1){
+        for(int i =0; i< 26; i++){
+            if(freq[i] != freq1[i]){
+                return false;
+            }
+        }
+        return true;
     }
 }
