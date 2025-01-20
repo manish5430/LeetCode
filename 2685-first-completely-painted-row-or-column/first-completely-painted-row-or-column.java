187a68802853ae7f79cb2ebd3b4d89d398fb07f1
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-20 20:45:04 +0530

    Time: 22 ms (80.60%) | Memory: 68.4 MB (47.26%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
@@ -1,28 +1,26 @@
 class Solution {
     public int firstCompleteIndex(int[] arr, int[][] mat) {
+        HashMap<Integer, int[]> map = new HashMap<>();
         int m = mat.length;
         int n = mat[0].length;
-        int[] rowCount = new int[m];
-        int[] colCount = new int[n];
 
-        HashMap<Integer, int[]> map = new HashMap<>();
         for(int i = 0; i< m; i++){
             for(int j = 0; j< n; j++){
-                map.put(mat[i][j], new int[] {i, j});
+                map.put(mat[i][j], new int[]{i, j});
             }
         }
 
-        int len = m * n;
+        int len = m * n;  // length of array 
+        int[] freq1 = new int[m];
+        int[] freq2 = new int[n];
         for(int i = 0; i< len; i++){
             int[] cell = map.get(arr[i]);
-            rowCount[cell[0]]++;
-            colCount[cell[1]]++;
-            
-            // comparing it with number of column present in that single row and col too 
-            // no.of colums in one row == n
-            if(rowCount[cell[0]] == n || colCount[cell[1]] == m){
+            freq1[cell[0]]++;
+            freq2[cell[1]]++;
+
+            if(freq1[cell[0]] == n || freq2[cell[1]] == m){
                 return i;
-            }
+            }            
         }
         return -1;
     }
