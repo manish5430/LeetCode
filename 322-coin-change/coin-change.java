d74f816910368ac4aee03962ad65f83562610521
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-22 20:25:52 +0530

    Time: 75 ms (10.92%) | Memory: 44.8 MB (45.58%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,28 +1,31 @@
 class Solution {
     public int coinChange(int[] nums, int amount) {
         int n = nums.length;
-        int sum = amount;
-        int[][] dp = new int[n+1][sum +1];
-        for(int i = 1; i< n+1; i++){
-            dp[i][0] = 0;
-        }      
-        for(int j = 0; j< sum+1; j++){
-            dp[0][j] = Integer.MAX_VALUE -1;
+        int target = amount;
+        int[][] dp = new int[n+1][target +1];
+        for(int[] num : dp){
+            Arrays.fill(num, -1);
         }
 
-        for(int i = 1; i< n+1; i++){
-            for(int j = 1; j< sum +1; j++){
-                if(nums[i-1] <= j){
-                    dp[i][j] = Math.min( 1 + dp[i][j - nums[i-1]], dp[i-1][j]);
-                }else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
+        int ans = recursion(nums, n-1, target, dp);
+        if(ans >= (int)Math.pow(10, 9)) return -1;
+        return ans;
+    }
+
+    static int recursion(int[] nums, int i, int target, int[][] dp){
+        if(i == 0){
+            if(target % nums[0] == 0) return target/ nums[0];  
+            else return (int)Math.pow(10, 9);
         }
 
-        if(dp[n][sum] == Integer.MAX_VALUE -1){
-            return -1;
+        if(dp[i][target] != -1) return dp[i][target];
+
+        int notake = 0 + recursion(nums, i-1, target, dp);
+        int take = (int)Math.pow(10, 9);
+        if(nums[i] <= target){
+            take = 1 + recursion(nums, i, target -nums[i], dp);
         }
-        return dp[n][sum];
+
+        return dp[i][target] = Math.min(take, notake); 
     }
 }
