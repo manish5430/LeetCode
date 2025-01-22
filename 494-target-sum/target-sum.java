b9b3620d4c246afcc79ceabfcb509a87300103bf
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-22 14:08:54 +0530

    Time: 5 ms (90.78%) | Memory: 44.5 MB (33.90%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,20 +1,28 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-
-        return recursion(nums, target, 0, 0);       
-        
-    }
-
-    static int recursion(int[] nums, int target, int index, int sum){
-        if(index >= nums.length){
-            if(sum == target) return 1;
-            return 0;
+        int n = nums.length;
+        int sum = 0;
+        for(int num : nums){
+            sum += num;
         }
 
-        int sub = recursion(nums, target, index +1, sum - nums[index]);
-        int add = recursion(nums, target, index +1, sum + nums[index]);
-
-        return sub + add;
+        if((sum + target) %2 != 0  || target > sum || target < -sum)  return 0;
+        int s1 = (sum + target) /2;
         
+        int[][] dp = new int[n+1][s1+1];
+        for(int i = 0; i< n+1; i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i = 1; i< n+1; i++){
+            for(int j = 0; j< s1 +1; j++){
+                if(nums[i-1] <= j){
+                    dp[i][j] = dp[i-1][j - nums[i-1]] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][s1];
     }
 }
