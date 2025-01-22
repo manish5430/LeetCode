ced9c1b0ee056790b21c295c42da1d47a0d4f66e
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-22 20:17:37 +0530

    Time: 16 ms (59.49%) | Memory: 44.9 MB (36.65%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -3,18 +3,17 @@ class Solution {
         int n = nums.length;
         int sum = amount;
         int[][] dp = new int[n+1][sum +1];
+        for(int i = 1; i< n+1; i++){
+            dp[i][0] = 0;
+        }      
         for(int j = 0; j< sum+1; j++){
             dp[0][j] = Integer.MAX_VALUE -1;
         }
-        for(int i = 1; i< n+1; i++){
-            dp[i][0] = 0;            
-        }
-
 
         for(int i = 1; i< n+1; i++){
-            for(int j = 1; j< sum+1; j++){
+            for(int j = 1; j< sum +1; j++){
                 if(nums[i-1] <= j){
-                    dp[i][j] = Math.min(1 + dp[i][j - nums[i-1]], dp[i-1][j]);
+                    dp[i][j] = Math.min( 1 + dp[i][j - nums[i-1]], dp[i-1][j]);
                 }else{
                     dp[i][j] = dp[i-1][j];
                 }
@@ -24,6 +23,6 @@ class Solution {
         if(dp[n][sum] == Integer.MAX_VALUE -1){
             return -1;
         }
-        return dp[n][sum];        
+        return dp[n][sum];
     }
 }
