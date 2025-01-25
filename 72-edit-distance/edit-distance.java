361b35c9f69b0d90dddd1a92cde1e128560146fa
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-26 00:32:49 +0530

    Time: 5 ms (63.47%) | Memory: 45 MB (61.74%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -4,12 +4,8 @@ class Solution {
         int m = s2.length();
         int[][] dp = new int[n+1][m+1];
 
-        for(int i = 0; i< n+1; i++){   // j == 0 s2 is empty so need to delete all char
-            dp[i][0] = i;
-        }
-        for(int j = 0; j< m+1; j++){  // i == 0 need to insert all characters
-            dp[0][j] = j;
-        }
+        for(int i = 0; i< n+1; i++){  dp[i][0] = i; }
+        for(int j = 0; j< m+1; j++){  dp[0][j] = j; }
 
         for(int i = 1; i< n+1; i++){
             for(int j = 1; j< m+1; j++){
