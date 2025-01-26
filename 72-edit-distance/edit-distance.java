5a7f558ab5828bed6df3961bd6cb4377d0a35555
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-26 09:15:45 +0530

    Time: 5 ms (63.43%) | Memory: 45.2 MB (31.74%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -3,21 +3,20 @@ class Solution {
         int n = s1.length();
         int m = s2.length();
         int[][] dp = new int[n+1][m+1];
-
+        
         for(int i = 0; i< n+1; i++){  dp[i][0] = i; }
         for(int j = 0; j< m+1; j++){  dp[0][j] = j; }
 
         for(int i = 1; i< n+1; i++){
             for(int j = 1; j< m+1; j++){
                 if(s1.charAt(i-1) == s2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
+                    dp[i][j] = dp[i-1][j-1];   // no action performed
                 }
                 else{
-                    dp[i][j] = 1 + Math.min(dp[i-1][j], 
-                                   Math.min(dp[i][j-1], dp[i-1][j-1]));
+                    dp[i][j] = 1 + Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1]));
                 }
             }
         }
-        return dp[n][m];
+        return dp[n][m];        
     }
 }
