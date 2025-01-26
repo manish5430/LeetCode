b3e2ab7de012591203dc3467673b269a30569956
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-26 10:23:36 +0530

    Time: 24 ms (30.81%) | Memory: 44.6 MB (78.63%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -9,10 +9,13 @@ class Solution {
             dp[i][0] = false;
         }
 
+// p can only match if all characters in p UP TO position j are '*'.
         for(int j = 1; j< m+1; j++){
-            if(p.charAt(j-1) == '*') dp[0][j] = dp[0][j-1]; 
-            else dp[0][j] = false;
-        }
+            if(p.charAt(j-1) != '*'){
+                dp[0][j] = false;
+            }
+            else dp[0][j] = dp[0][j-1];  // if all the char are * keep using true 
+        }                             // if any one is not * make it false and proceed with it
 
         for(int i = 1; i< n+1; i++){
             for(int j = 1; j< m+1; j++){
