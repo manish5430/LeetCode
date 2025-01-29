06fe3804b899448963642932558b761e9eea719e
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-01-29 13:30:03 +0530

    Time: 6 ms (98.46%) | Memory: 57.1 MB (30.62%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -4,33 +4,33 @@ class Solution {
         ArrayList<String> list = new ArrayList<>();
 
         recursion(s, 0, result, list);
-        return result;        
+        return result;
     }
 
-    static void recursion(String s, int i, List<List<String>> result,
-                                            ArrayList<String> list){
+    static void recursion(String s, int i, List<List<String>> result, ArrayList<String> list){
+        int n = s.length();
         if(i == s.length()){
             result.add(new ArrayList<>(list));
             return;
-        }          
+        }
 
-        for(int j = i; j< s.length(); j++){
+        for(int j = i; j< n; j++){
             if(isPalindrome(s, i, j)){
                 list.add(s.substring(i, j+1));
-                
+
                 recursion(s, j+1, result, list);
                 list.remove(list.size() -1);
             }
-        }                         
+        }
     }
 
     static boolean isPalindrome(String s, int i, int j){
-        while(i< j){
-            if(s.charAt(i) != s.charAt(j)){
-                return false;
+        while(i < j){
+            if(s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
             }
-            i++;
-            j--;
+            else return false;
         }
         return true;
     }
