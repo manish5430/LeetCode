75cb96bc1a4e772614543258a1c08a047fa13885
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-02-08 11:00:21 +0530

    Time: 26 ms (41.94%) | Memory: 47.8 MB (76.04%) - LeetSync

--- 2954-maximum-sum-of-almost-unique-subarray/maximum-sum-of-almost-unique-subarray.java
+++ 2954-maximum-sum-of-almost-unique-subarray/maximum-sum-of-almost-unique-subarray.java
@@ -1,24 +1,20 @@
 class Solution {
     public long maxSum(List<Integer> nums, int m, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        //int ans = 0;
-        //int sum = 0;
         long ans = 0;
         long sum = 0;
-
-        for(int i = 0; i< k; i++){    // first sliding window 
+        for(int i = 0; i< k; i++){
             map.put(nums.get(i), map.getOrDefault(nums.get(i), 0) +1);
             sum += nums.get(i);
         }
-
-        if(map.size() >= m)  ans = sum;
+        if(map.size() >= m) ans = sum;
 
         int left = 0;
-        int right = k;
+        int right = k;     // has to be of length k 
+
         while(right < nums.size()){
-            
             map.put(nums.get(left), map.getOrDefault(nums.get(left), 0) -1);
-            if(map.get(nums.get(left)) == 0)  map.remove(nums.get(left));
+            if(map.get(nums.get(left)) == 0) map.remove(nums.get(left));
             sum -= nums.get(left);
             left++;
 
@@ -26,11 +22,10 @@ class Solution {
             sum += nums.get(right);
             right++;
 
-            if(map.size() >= m){
-                ans = Math.max(ans, sum);
-            }
+            if(map.size() >= m) ans = Math.max(sum, ans);           
+
         }
 
-        return ans;        
+        return ans;
     }
 }
