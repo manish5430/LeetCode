96cafd7923aba425c89a9bec7854de5884c584dd
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-02-10 00:51:37 +0530

    Time: 28 ms (90.23%) | Memory: 59.6 MB (42.24%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,35 +1,33 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
-        int[] result = new int[n -k+1];
+        int[] result = new int[n- k+1];
+        Deque<Integer> deque = new ArrayDeque<>();
+
         if(nums.length == 0) return result;
 
-    // to store indices only to add or remove
-        Deque<Integer> deque = new ArrayDeque<>();  
-        int index = 0;
+        int index = 0;     // initial window only 
         while(index < k){
-            // if the new is larger than previous one remove all of them 
             while(! deque.isEmpty() && nums[deque.peekLast()] <= nums[index]){
                 deque.pollLast();
             }
-            deque.offerLast(index);  // add the new larger index only 
-            index++;                 // just for initial window 
+            deque.offerLast(index);
+            index++; 
         }
         result[0] = nums[deque.peekFirst()];
 
-        for(int i= 1; i< n - k+1; i++){
-            // removing left indices as deque only contains indices 
+        for(int i = 1; i< n-k+1; i++){
             if(! deque.isEmpty() && deque.peekFirst() < i){
                 deque.pollFirst();
             }
-            // right element is i + k-1
-            while(! deque.isEmpty() && nums[deque.peekLast()] <= nums[i +k-1]){
+
+            while(! deque.isEmpty() && nums[deque.peekLast()] <= nums[i+k -1]){
                 deque.pollLast();
             }
-            deque.offerLast(i +k-1);
+            deque.offerLast(i+k -1);
             result[i] = nums[deque.peekFirst()];
         }
-        
+
         return result;
 
     }
