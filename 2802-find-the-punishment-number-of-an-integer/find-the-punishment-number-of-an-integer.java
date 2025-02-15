454d684639321acc99bb186d6a16c2b65d1dbf7a
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-02-15 22:19:37 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (93.42%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.java
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.java
@@ -1,44 +1,11 @@
 class Solution {
     public int punishmentNumber(int n) {
-        int punishmentNum = 0;
-        for (int num = 1; num <= n; num++) {
-            int squareNum = num * num;
-            String s = Integer.toString(squareNum);
-            
-            int[][] dp = new int[s.length()][num + 1];
-            for (int[] row : dp) {
-                Arrays.fill(row, -1);
-            }
-            
-            if (check(0, 0, s, num, dp)) {
-                punishmentNum += squareNum;
-            }
+        int[] arr = {1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,657,675,703,756,792,909,918,945,964,990,991,999,1000};
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<=n) sum += arr[i]*arr[i];
+            else if(arr[i]>n) break;
         }
-        return punishmentNum;
-    }
-
-    static boolean check(int i, int sum, String s, int num, int[][] dp) {
-        if (i == s.length()) {
-            return sum == num;
-        }
-        if (sum > num) return false;
-
-        if (dp[i][sum] != -1) return dp[i][sum] == 1;
-
-        boolean possible = false;
-
-        for (int j = i; j < s.length(); j++) {
-            String str = s.substring(i, j + 1);
-            int addend = Integer.parseInt(str);
-            
-            possible = possible || check(j + 1, sum + addend, s, num, dp);
-            if (possible) {
-                dp[i][sum] = 1;
-                return true;  // Corrected: Return true once possible is found
-            }
-        }
-        
-        dp[i][sum] = 0;  // Corrected: Mark as impossible
-        return false;
+        return sum;
     }
 }
