80df2b664be40f949af3e621b01db8964cf11558
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-02-20 11:51:33 +0530

    Time: 494 ms (5.09%) | Memory: 59.9 MB (5.09%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,24 +1,25 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
         int n = nums[0].length();
-        ArrayList<String> list = new ArrayList<>();
-        recursion("", n, list);
+        HashSet<String> set = new HashSet<>();
+        recursion("", n, set);
 
         for(String s1 : nums){
-            if(list.contains(s1)){
-                list.remove(s1);
+            if(set.contains(s1)){
+                set.remove(s1);
             }
         }
-        return list.get(0);        
+        ArrayList<String> list = new ArrayList<>(set);
+        return list.get(0);            
     }
 
-    static void recursion(String s, int n, ArrayList<String> list){
+    static void recursion(String s, int n, HashSet<String> set){
         if(n == 0){
-            list.add(s);
+            set.add(s);
             return;
         }
 
-        recursion(s + "0", n-1, list);
-        recursion(s + "1", n-1, list);
+        recursion(s + "0", n-1, set);
+        recursion(s + "1", n-1, set);
     }
 }
