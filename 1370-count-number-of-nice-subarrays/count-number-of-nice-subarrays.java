267355fe9e1b93b21ffb7636efce2d110d26a156
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-02-26 15:28:24 +0530

    Time: 45 ms (7.41%) | Memory: 55.3 MB (44.96%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,23 +1,23 @@
 class Solution {
-    public int numberOfSubarrays(int[] nums, int k) {
-        int[] prefixSum = new int[nums.length];
-        for(int i = 0; i< nums.length; i++){
-            if(nums[i] %2 == 0) prefixSum[i] = 0;
-            else prefixSum[i] = 1;
-        }  
+    public int numberOfSubarrays(int[] arr, int k) {
+        int[] nums = new int[arr.length];
+        for(int i = 0; i< arr.length; i++){
+            if(arr[i] %2 != 0) nums[i] = 1;
+            else nums[i] = 0;
+        }
 
-        int[] freq = new int[nums.length +1];
-        freq[0] = 1;
-        int sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int prefixSum = 0;
         int count = 0;
 
-        for(int i = 0; i< prefixSum.length; i++){
-            sum += prefixSum[i];
+        for(int num : nums){
+            prefixSum += num;
 
-            if(sum >= k){
-                count = count + freq[sum -k];
-            }
-            freq[sum]++;
+            if(prefixSum == k) count++;
+
+            if(map.containsKey(prefixSum - k)) count += map.get(prefixSum- k);
+
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) +1);
         }
         return count;
     }
