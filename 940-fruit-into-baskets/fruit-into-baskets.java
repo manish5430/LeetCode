1570e66d2f5e56ee4367dbc2f07fe4a022b03794
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-03-02 17:49:39 +0530

    Time: 50 ms (62.49%) | Memory: 55.3 MB (41.66%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -4,19 +4,18 @@ class Solution {
         int i = 0;
         int j = 0;
         int max = 0;
-
+        
         while(j < nums.length){
             map.put(nums[j], map.getOrDefault(nums[j], 0) +1);
 
-            if(map.size() <= 2) max = Math.max(max, j-i+1);
-            else if(map.size() >2){
-                while(map.size() >2){
-                    map.put(nums[i], map.getOrDefault(nums[i], 0) -1);
-                    if(map.get(nums[i]) == 0) map.remove(nums[i]);
-                    i++;
-                }
+            while(map.size() > 2){
+                map.put(nums[i], map.getOrDefault(nums[i], 0) -1);
+
+                if(map.get(nums[i]) == 0) map.remove(nums[i]);
+                i++;
             }
-            //if(map.size() <= 2) max = Math.max(max, j-i+1);
+
+            if(map.size() <= 2)  max = Math.max(max, j-i+1);
             j++;
         }
         return max;
