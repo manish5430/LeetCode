48535f3366bcd8208a826d93b8ff8f4f37e35a3e
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-03-02 23:33:20 +0530

    Time: 50 ms (34.33%) | Memory: 47.7 MB (44.55%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -2,46 +2,26 @@ class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
 
         return recursion(nums, k) - recursion(nums, k-1);
-        
     }
 
     static int recursion(int[] nums, int k){
         HashMap<Integer, Integer> map = new HashMap<>();
-        int left = 0;
-        int right = 0;
+        int i = 0;
+        int j = 0;
         int count = 0;
-        
-        while(right < nums.length){
-            map.put(nums[right], map.getOrDefault(nums[right], 0) +1);
-            
+
+        while(j< nums.length){
+            map.put(nums[j], map.getOrDefault(nums[j], 0) +1);
+
             while(map.size() > k){
-                map.put(nums[left], map.getOrDefault(nums[left], 0) -1);
-                if(map.get(nums[left]) == 0){
-                    map.remove(nums[left]);
-                }
-                left++;
+                map.put(nums[i], map.getOrDefault(nums[i], 0) -1);
+                if(map.get(nums[i]) == 0) map.remove(nums[i]);
+                i++;
             }
-        
-            // if(map.size() == k){
-            //     int templeft = left;
-            //     while(map.size() == k){
-            //         count++;
 
-            //         map.put(nums[templeft], map.getOrDefault(nums[templeft], 0) -1);
-            //         if(map.get(nums[templeft]) == 0){
-            //             map.remove(nums[templeft]);
-            //         }
-            //         templeft++;
-            //     }
-                
-            //     for(int i = left; i < templeft; i++){
-            //         map.put(nums[i], map.getOrDefault(nums[i], 0) +1);
-            //     }            
-            // }
-
-            count += (right - left + 1);
-            right++;  
-        }   
-        return count;   
+            count += (j -i +1);
+            j++;
+        }
+        return count;
     }
 }
