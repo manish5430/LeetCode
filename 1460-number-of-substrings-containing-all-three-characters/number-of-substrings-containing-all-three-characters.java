13f0c11076cf753a8cbaa96526f14b604d09af20
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-03-11 20:57:52 +0530

    Time: 39 ms (18.64%) | Memory: 45.2 MB (14.42%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -2,20 +2,35 @@ class Solution {
     public int numberOfSubstrings(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
         int count = 0;
-        int i = 0; 
-        int j = 0;
+        int left = 0;
+        int right = 0;
         
-        while(j < s.length()){
-            map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) +1);
+        // this initail window logic fails what if starting ke characters repeated ho 
+        // it wont count them correctly 
+        
+        // for(int i = 0; i< s.length(); i++){
+        //     map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) +1);
+
+        //     if(map.size() == 3){
+        //         right = i+1;
+        //         count += (s.length() - i);
+        //         break;
+        //     }
+        // }
+        
+        while(right < s.length()){
+            map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) +1);
 
             while(map.size() == 3){
-                count += (s.length() - j);
+                count += (s.length() - right);
+                map.put(s.charAt(left), map.getOrDefault(s.charAt(left), 0) -1);
 
-                map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) -1);
-                if(map.get(s.charAt(i)) == 0) map.remove(s.charAt(i));
-                i++;
+                if(map.get(s.charAt(left)) == 0){
+                    map.remove(s.charAt(left));
+                }
+                left++;
             }
-            j++;
+            right++;            
         }
         return count;
     }
