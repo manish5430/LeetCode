6db5f1d6618c6747d5e1be776308cd96e61ee2bd
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-03-26 22:59:32 +0530

    Time: 107 ms (36.73%) | Memory: 118.8 MB (61.95%) - LeetSync

--- 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.java
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.java
@@ -12,24 +12,22 @@ class Solution {
             list1.add(new int[]{x1, x2});
             list2.add(new int[]{y1, y2});
         }
-        
-        int[][] nums1 = list1.toArray(new int[list1.size()][]);
-        int[][] nums2 = list2.toArray(new int[list2.size()][]);
 
-        int[][] result1 = merge(nums1);
-        int[][] result2 = merge(nums2);
+        int[][] arr1 = list1.toArray(new int[list1.size()][]);
+        int[][] arr2 = list2.toArray(new int[list2.size()][]);
 
-        return result1.length >= 3  || result2.length >= 3;
-        
-        
+        int[][] result1 = merge(arr1);
+        int[][] result2 = merge(arr2);
+
+        return result1.length  >= 3  ||  result2.length >= 3;
+    
     }
 
-    public int[][] merge(int[][] nums) {
+    static int[][] merge(int[][] nums){
         int n = nums.length;
-
-        Arrays.sort(nums, (a, b) -> a[0] - b[0]);
-
         ArrayList<int[]> list = new ArrayList<>();
+
+        Arrays.sort(nums, (a,b) -> a[0] - b[0]);
         list.add(nums[0]);
 
         for(int i = 1; i< n; i++){
@@ -40,7 +38,7 @@ class Solution {
                 list.add(nums[i]);
             }
         }
-        int[][] ans = list.toArray(new int[list.size()][]);
+        int[][] ans =  list.toArray(new int[list.size()][]);
         return ans;
     }
 }
