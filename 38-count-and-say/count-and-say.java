2630e2c5b29bc4e4132b538d5eae403cd9c04dda
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-04-20 01:24:48 +0530

    Time: 2 ms (91.89%) | Memory: 41.6 MB (51.66%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,13 +1,12 @@
 class Solution {
     public String countAndSay(int n) {
-        
-        String result = recursion(n, new StringBuilder());
-        return result;       
 
+        String result = recursion(n, new StringBuilder());
+        return result;        
     }
 
     static String recursion(int n, StringBuilder sb){
-        if(n == 1){
+        if(n == 1){ 
             sb.append(1);
             return sb.toString();
         }
@@ -15,20 +14,16 @@ class Solution {
         String say = recursion(n-1, new StringBuilder());
 
         char[] arr = say.toCharArray();
-        //int count = 1;
-
         for(int i = 0; i< arr.length; i++){
-            char ch = arr[i];
             int count = 1;
-
-            while(i+1 < arr.length && arr[i] == arr[i+1]){
+            char c = arr[i];
+            while(i+1 < arr.length && c == arr[i+1]){
                 count++;
                 i++;
             }
-            sb.append(count).append(ch);
-            //count = 1;
-            
+            sb.append(count).append(arr[i]);
         }
+        
         return sb.toString();
     }
 }
