dbe7f0b4034e14f5649780f7f9ffd1888e4259c3
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-04-25 01:27:55 +0530

    Time: 12 ms (98.69%) | Memory: 55.7 MB (37.09%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -13,5 +13,9 @@ class Solution {
 
         if(count2 %2 == 0) return count2/2;
         return (count2 +1) /2;   
+
+        // see in the end it was all about the even odd count1 2
+        // ]]] [[[   this  is count2 = 3 and one swap fix 2 brackets= 4 signs 
+        //  for odd ones   count2 +1  /2
     }
 }
