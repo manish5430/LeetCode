4188bf14e15d8bf15372fc99eef90472921ab6cb
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-04-24 21:08:55 +0530

    Time: 1 ms (52.67%) | Memory: 41.7 MB (29.34%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -5,16 +5,12 @@ class Solution {
         Stack<Character> stack = new Stack<>();
         for(char c : s.toCharArray()){
             if(c == '(') stack.push('(');
-            else {
-                if(c == ')' && stack.isEmpty()){
-                    stack.push(')');
-                }
-                else if(c == ')' && !stack.isEmpty() && stack.peek() == '('){
+            else {// c == ')'
+                if(!stack.isEmpty() && stack.peek() == '('){
                     stack.pop();
                 }
-                else if(c == ')' && !stack.isEmpty() && stack.peek() == c){
-                    stack.push(')');
-                }
+                else  stack.push(')');
+                
             }
             
         }
