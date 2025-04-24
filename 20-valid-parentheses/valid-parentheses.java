8c38a688a15186dba2b32b32307f44fae9676861
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-04-24 15:14:34 +0530

    Time: 2 ms (97.40%) | Memory: 42.2 MB (12.70%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,6 +2,10 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
 
+        // see jab tak opening bracket hai tab tak simply push closing b of them in stack 
+        // else if closing bracket aaya to check if stack is empty or not || the last pushed one is same as this new closing bracket or not   IF NOT  return false simply 
+        // also its LIFO  last open first closed   jo sabse last mein open hoga vo sabse uper hoga stcak mein thats why stack.pop() != c works here 
+
         for(int c : s.toCharArray()){
             if( c == '(') stack.push(')');
             else if( c == '[') stack.push(']');
