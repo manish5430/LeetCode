e04a6f662c95d70ff9903742a6508671bde0a325
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-04-29 10:28:03 +0530

    Time: 2 ms (100.00%) | Memory: 61.7 MB (35.52%) - LeetSync

--- 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.java
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.java
@@ -1,29 +1,18 @@
 class Solution {
     public long countSubarrays(int[] nums, long k) {
-        HashMap<Integer, Long> map = new HashMap<>();
-
-        int left = 0;
         long count = 0;
         long sum = 0;
+        int left = 0;
 
         for(int right = 0; right< nums.length; right++){
             sum += nums[right];
-            int len = right - left +1;
-            long score = sum * len;
-
-            map.put(right, score);
             
-            while(left <= right && score >= k){
+            while(sum * (right - left+1)  >= k){
                 sum -= nums[left];
                 left++;
-                len = right - left +1;
-                score = sum * len;
             }
-
             count += (right - left +1);
         }
-
         return count;
-        
     }
 } 
