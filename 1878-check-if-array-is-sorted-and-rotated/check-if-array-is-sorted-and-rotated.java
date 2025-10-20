9f65cfd2d1b8ab45fabf66b99cc605d3b2e617a5
Manish Chauhan <163045433+manish5430@users.noreply.github.com>
2025-10-20 10:10:48 +0530

    Time: 1 ms (6.15%) | Memory: 41.1 MB (83.77%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -6,28 +6,31 @@ class Solution {
         int x = 0;
 
         for(int i = 0; i< n; i++){
-            if(nums[i] >= max) {
+            if(nums[i] >= max){
                 max = nums[i];
                 x = i;
             }
         }
 
         ArrayList<Integer> list = new ArrayList<>();
-
-        for(int i = x+1; i< n; i++){
+        for(int i = x +1; i< n; i++){
             list.add(nums[i]);
         }
+
         for(int i = 0; i<= x; i++){
             list.add(nums[i]);
         }
-
-        int k = list.size();
-        for(int i = 0; i< k-1; i++){
-            if(list.get(0) == list.get(k-1)) continue;
-            if(list.get(i) > list.get(i+1)) return false;
+        
+        for(int i = 0; i< n -1; i++){
+            if(list.get(0) == list.get(n-1)) continue;
+            if(list.get(i) > list.get(i+1)) {
+                return false;
+            }
         }
 
-        return true;      
+        return true;
+
+
         
     }
 }
